ccf9699316c95bde802729da82380d667a0f0b8a
Sable <78709084+Sqble@users.noreply.github.com>
2022-03-24 18:06:30 -0700

    changed 3D viewport to a class

--- Game/viewport3D.py
+++ Game/viewport3D.py
@@ -8,17 +8,37 @@ class Viewport3D:
     def display(player,rays,scale,fov):
         xStep = const.WIDTH // fov
 
-
-        pygame.draw.rect(screen, (173,216,230), pygame.Rect( (0,0), (const.WIDTH, const.HEIGHT // 2) ))
-
-        pygame.draw.rect(screen, (100,100,100), pygame.Rect( (0, const.HEIGHT // 2), (const.WIDTH, const.HEIGHT) ))
+        Viewport3D.drawSky()
+        Viewport3D.drawFloor()
 
         for i,ray in enumerate(rays):
-            distance = ray.distance * cos(radians(player.angle - ray.angle))
-            lineHeight = int(scale * const.HEIGHT / (distance+0.0001) )
-            if lineHeight > const.HEIGHT: lineHeight = const.HEIGHT
+            wallHeight = Viewport3D.calcWallHeight(scale, ray.distance, player.angle, ray.angle)
 
-            lineOffset = const.HEIGHT / 2 - lineHeight/2
+            lineOffset = Viewport3D.heightBeforeWall(wallHeight)
             
-            color = (0,255,0) if ray.type == 'x' else (0,200,0)
-            pygame.draw.rect(screen, color, pygame.Rect( (i * xStep, lineOffset), (xStep, lineHeight) ))
+            color = Viewport3D.shade(ray.type)
+            
+            pygame.draw.rect(screen, color, pygame.Rect( (i * xStep, lineOffset), (xStep, wallHeight) ))
+    
+
+
+    def drawSky():
+        pygame.draw.rect(screen, (173,216,230), pygame.Rect( (0,0), (const.WIDTH, const.HEIGHT // 2) ))
+    
+    def drawFloor():
+        pygame.draw.rect(screen, (100,100,100), pygame.Rect( (0, const.HEIGHT // 2), (const.WIDTH, const.HEIGHT) ))
+    
+    def removeFisheye(distance,angle1, angle2):
+        return distance * cos(radians(angle1 - angle2))
+    
+    def calcWallHeight(scale, distance, angle1, angle2):
+        distance = Viewport3D.removeFisheye(distance, angle1, angle2)
+        wallHeight = int(scale * const.HEIGHT / (distance+0.0001) )
+        if wallHeight > const.HEIGHT: wallHeight = const.HEIGHT
+        return wallHeight
+
+    def heightBeforeWall(wallHeight):
+        return const.HEIGHT / 2 - wallHeight/2
+    
+    def shade(rayType):
+        return (0,255,0) if rayType == 'x' else (0,200,0)
